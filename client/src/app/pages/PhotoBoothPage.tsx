3afb284d5d0bae99bf2cd9a0c0e13b923071dab5
Jane Yang <janeyang@Janes-MacBook-Pro.local>
2025-10-26 21:20:42 -0700

    Finalize photobooth mvp

--- client/src/app/pages/PhotoBoothPage.tsx
+++ client/src/app/pages/PhotoBoothPage.tsx
@@ -1,9 +1,11 @@
 import PhotoBooth from "../../components/PhotoBooth";
+import PhotoboothText from "../../assets/PhotoboothText.gif";
 
 export default function PhotoBoothPage(): React.ReactNode {
   return (
-    <>
+    <div className="flex flex-col items-center my-20 gap-4">
+      <img src={PhotoboothText} alt="Photobooth Text" className="w-50" />
       <PhotoBooth />
-    </>
+    </div>
   );
 }
--- client/src/components/PhotoBooth.tsx
+++ client/src/components/PhotoBooth.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useRef, useState } from "react";
-import { PrinterIcon, ArrowPathIcon } from "@heroicons/react/24/outline";
+import {
+  PrinterIcon,
+  ArrowPathIcon,
+  CameraIcon,
+} from "@heroicons/react/24/outline";
+import download from "../utils/download";
 
 interface WebcamProps {
   reset?: boolean;
 }
 
-async function formatPhotoStrip(images: string[]) {
+async function formatPhotoStrip(images: string[]): Promise<string> {
   const bigImage = await Promise.all(
     images.map(
       (img) =>
@@ -41,38 +46,58 @@ async function formatPhotoStrip(images: string[]) {
     ctx!.drawImage(img, x, y, imgWidth, imgHeight);
   });
 
-  const link = document.createElement("a");
-  link.download = "photostrip.png";
-  link.href = photostrip.toDataURL("image/png", 1.0);
-  link.click();
+  return photostrip.toDataURL("image/png", 1.0);
+
+  // const link = document.createElement("a");
+  // link.download = "photostrip.png";
+  // link.href = photostrip.toDataURL("image/png", 1.0);
+  // link.click();
 }
 
 export default function Webcam(props: WebcamProps): React.ReactNode {
   const { reset = false } = props;
+  // const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+  const mediaStreamRef = useRef<MediaStream | null>(null);
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
-
   const [imgArr, setImgArr] = useState<string[]>([]);
+  const [formattedImgStrip, setFormattedImgStrip] = useState<string | null>(
+    null
+  );
 
-  useEffect(() => {
-    const startCamera = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-        });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-        }
-        setMediaStream(stream);
-      } catch (error) {
-        console.error("ERROR", error);
+  const [ready, setReady] = useState<boolean>(false);
+
+  const startCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
       }
-    };
+      mediaStreamRef.current = stream;
+      // setMediaStream(stream);
+    } catch (error) {
+      console.error("ERROR", error);
+    }
+  };
 
-    startCamera();
-  }, []);
+  const stopCamera = () => {
+    const stream = mediaStreamRef.current;
+    if (videoRef.current) {
+      // Detach video first
+      videoRef.current.pause();
+      videoRef.current.srcObject = null;
+    }
+
+    if (stream) {
+      stream.getTracks().forEach((track) => {
+        track.stop();
+      });
+    }
+    mediaStreamRef.current = null;
+  };
 
   const captureImage = () => {
     if (videoRef.current && canvasRef.current) {
@@ -82,12 +107,6 @@ export default function Webcam(props: WebcamProps): React.ReactNode {
 
       // set canvas size to video size
       if (context && video.videoWidth && video.videoHeight) {
-        console.log(
-          "I AM IN A;LDSKFJA;SDKL",
-          video,
-          video.videoWidth,
-          video.videoHeight
-        );
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
 
@@ -104,7 +123,11 @@ export default function Webcam(props: WebcamProps): React.ReactNode {
   };
 
   const resetWebcam = () => {
+    stopCamera();
     setImgArr([]);
+    setReady(false);
+    setFormattedImgStrip(null);
+    startCamera();
 
     // reset flash animation
     const el = document.getElementById("flash");
@@ -113,20 +136,38 @@ export default function Webcam(props: WebcamProps): React.ReactNode {
     el?.classList.add("webcam-flash");
   };
 
+  // on page load, start camera
+  useEffect(() => {
+    startCamera();
+  }, []);
+
   useEffect(() => {
     if (reset) resetWebcam();
   }, [reset]);
 
   useEffect(() => {
-    if (imgArr.length <= 3) {
-      setTimeout(() => captureImage(), 3000);
+    console.log("in the ready imgArr", imgArr, ready, formattedImgStrip);
+    if (ready) {
+      console.log(imgArr.length);
+      if (imgArr.length <= 3) {
+        setTimeout(() => captureImage(), 3000);
+      } else {
+        formatPhotoStrip(imgArr).then((strip) => {
+          setFormattedImgStrip(strip);
+        });
+
+        stopCamera();
+      }
     }
-  }, [imgArr]);
+  }, [imgArr, ready, setFormattedImgStrip]);
+
+  useEffect(() => () => stopCamera(), []);
 
   return (
-    <div className="flex items-center justify-center my-20 flex-col">
-      {videoRef && (
-        // mirrored image
+    <div className="flex items-center justify-center flex-col gap-4">
+      {formattedImgStrip ? (
+        <img src={formattedImgStrip} alt="Photo Strip" className="w-50" />
+      ) : (
         <div className="w-50 scale-x-[-1] ">
           <video ref={videoRef} autoPlay muted />
           <div
@@ -137,15 +178,32 @@ export default function Webcam(props: WebcamProps): React.ReactNode {
         </div>
       )}
       <div className="flex gap-4">
-        <button className="hover:cursor-pointer" onClick={() => resetWebcam()}>
-          <ArrowPathIcon className="w-4" />
-        </button>
-        <button
-          className="hover:cursor-pointer"
-          onClick={() => formatPhotoStrip(imgArr)}
-        >
-          <PrinterIcon className="w-4" />
-        </button>
+        {formattedImgStrip ? (
+          <div className="flex gap-2">
+            <button
+              className="hover:cursor-pointer"
+              onClick={() => resetWebcam()}
+            >
+              <ArrowPathIcon className="w-4" />
+            </button>
+            <button
+              className="hover:cursor-pointer"
+              onClick={() => {
+                if (formattedImgStrip)
+                  download(formattedImgStrip, "photostrip.png");
+              }}
+            >
+              <PrinterIcon className="w-4" />
+            </button>
+          </div>
+        ) : (
+          <button
+            className="hover:cursor-pointer"
+            onClick={() => setReady(true)}
+          >
+            <CameraIcon className="w-4" />
+          </button>
+        )}
       </div>
     </div>
   );
